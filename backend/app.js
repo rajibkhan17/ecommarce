6e730485a9326d7b7dcd1b303e70e2e8155c074b
rajibkhan17 <rajibkhan8189@gmail.com>
2021-06-07 08:12:24 +0600

    first route created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+//import all route
+const products = require('./routes/product');
 
 
-
+app.use('/api/vi', products)
 
 
 module.exports = app
