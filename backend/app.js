8535a1ffd0d68cf1f1fbe39db39a789eb51fab8c
rajibkhan17 <rajibkhan8189@gmail.com>
2021-06-10 07:33:35 +0600

    new product route created

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const products = require('./routes/product');
 
 
-app.use('/api/vi', products)
+app.use('/api/v1', products)
 
 
 module.exports = app
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,19 @@
+const Product = require('../models/product')
 
+// test route : =>  /api/v1//products
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
         message: 'this product show all product in database' 
+    })
+}
+
+//create new product: => /api/v1/product/new
+exports.newProduct = async (req, res, next) => {
+
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
     })
 }
