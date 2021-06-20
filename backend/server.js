95f82344aa2ce73504a0e15ee4eea434597b0fcd
rajibkhan17 <rajibkhan8189@gmail.com>
2021-06-20 09:44:32 +0600

    Handle Uncaught Exceptions

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require('./app')
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database')
 
+//handle Uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('shutting down due to uncaught exception');
+    process.exit(1)
+})
+
 //setting up config file
 dotenv.config({ path: 'backend/config/config.env'})
 
