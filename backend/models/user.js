e32f1579a2538330d31e0da21e79cfca8434fce2
rajibkhan17 <rajibkhan8189@gmail.com>
2021-07-29 10:15:13 +0600

    password encrypt

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 
 const userSchema = new mongoose.Schema({
@@ -42,5 +43,15 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpire: Date
 
 })
+
+//Entrypting password before saving 
+userSchema.pre('save', async function (next){
+    if(!this.isDirectModified('password')){
+        next()
+    }
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
 
 module.exports = mongoose.model('User', userSchema);
